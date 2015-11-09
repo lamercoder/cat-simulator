8ba1382b1e91733bd1e481ac412eec4a2ea97b56
L@m3rC0d3r <lamercoder@xolus.net>
2015-11-09 03:03:37 +0100

    How to do proper exception handling

---
+++ Test2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+class Test2 {
+
+	public static void main(String argv[]) {
+		try {
+			int c = readOneChar();
+			System.out.println("The character " + c + "was read");		
+		} catch (FileNotFoundException e) {
+			System.out.println("File did not exist");
+		} catch (IOException e) {
+			System.out.println("Well i could not read, sorry");
+		}
+	}
+
+	public static int readOneChar() throws IOException {
+		return openTempFile().read();
+	}
+
+	public static InputStreamReader openTempFile() throws FileNotFoundException {
+		return new FileReader("./temp");
+	}
+
+
+}
